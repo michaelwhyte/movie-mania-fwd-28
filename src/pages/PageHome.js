6a8b29a43c546ed52d80548fb886d2cca8d23777
Michael Whyte <info@mwhyte.ca>
2021-08-17 15:02:14 -0700

    Add TMDB API call to Home page

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,8 +1,33 @@
+import { useEffect } from 'react';
 import NavSort from '../components/NavSort';
 import Movies from '../components/Movies';
+import { API_TOKEN} from '../globals/globals';
 
 function PageHome({ sort }) {
 
+    // https://api.themoviedb.org/3/movie/popular?api_key=123&language=en-US&page=1
+
+    useEffect(() => {
+
+        const fetchMovies = async () => {
+
+            const res = await fetch(`https://api.themoviedb.org/3/movie/${sort}?&language=en-US&page=1`, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + API_TOKEN
+                }
+            });
+            let rawMovieData = await res.json();
+            rawMovieData = rawMovieData.results.splice(0, 12);
+            console.log(rawMovieData);
+
+        }
+
+        fetchMovies();
+
+
+    }, [sort]);
 
 
     return (
