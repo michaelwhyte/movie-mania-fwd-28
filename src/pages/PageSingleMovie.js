66e1b8c340e5f8c191ed8f746ff418ce8faf10be
Michael Whyte <info@mwhyte.ca>
2021-08-17 10:33:20 -0700

    Create initial pages

---
+++ src/pages/PageSingleMovie.js
@@ -0,0 +1,9 @@
+function PageSingleMovie() {
+    return (
+        <section className="home-page">
+            ...this is the single movie page...
+        </section>
+    )
+}
+
+export default PageSingleMovie;
