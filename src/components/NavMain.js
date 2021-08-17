dfb7713e3c64fd3ff2435cb37ff0aac00852dcc9
Michael Whyte <info@mwhyte.ca>
2021-08-17 11:12:19 -0700

    Create NavMain.js

---
+++ src/components/NavMain.js
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+
+function NavMain() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about">About</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/favs">Favs</NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavMain;
+
+
